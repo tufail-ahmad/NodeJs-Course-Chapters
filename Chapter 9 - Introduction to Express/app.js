20feabde5d354bdc2ae191939c3b3c9a0366c61e
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-06-02 08:52:09 +0530

    chapter 9 has been completed

---
+++ Chapter 9 - Introduction to Express/app.js
@@ -0,0 +1,29 @@
+// External Modules
+const express = require("express");
+
+// Internal/Local Modules
+const handler = require("./user");
+// Create a server instance and pass the request handler
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  console.log("came in first middleware", req.url, req.method);
+  //res.send("<p>Came from first middleware</p>");
+  next();
+});
+
+app.post("/submit-details", (req, res, next) => {
+  console.log("came in second middleware", req.url, req.method);
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("came in another middleware", req.url, req.method);
+  res.send("<p>Came from another middleware</p>");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on address http://localhost:${PORT}`);
+});
