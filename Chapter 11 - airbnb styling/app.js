1cc2dc2a5872cf4467663d8f37f738f1d6ae234b
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-06-05 08:55:05 +0530

    updated

--- Chapter 11 - airbnb styling/app.js
+++ Chapter 11 - airbnb styling/app.js
@@ -1,9 +1,12 @@
-const express = require("express");
-const path = require("path");
+// Core Module
+const path = require('path');
 
-// local module
-const userRouter = require("./routes/userRouter");
-const hostRouter = require("./routes/hostRouter");
+// External Module
+const express = require('express');
+
+//Local Module
+const userRouter = require("./routes/userRouter")
+const hostRouter = require("./routes/hostRouter")
 const rootDir = require("./utils/pathUtil");
 
 const app = express();
@@ -11,13 +14,14 @@ const app = express();
 app.use(express.urlencoded());
 app.use(userRouter);
 app.use("/host", hostRouter);
-app.use(express.static(path.join(rootDir, "public")));
+
+app.use(express.static(path.join(rootDir, 'public')))
 
 app.use((req, res, next) => {
-  res.status(400).sendFile(path.join(rootDir, "views", "404.html"));
-});
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on address http://localhost:${PORT}`);
 });
--- Chapter 11 - airbnb styling/routes/hostRouter.js
+++ Chapter 11 - airbnb styling/routes/hostRouter.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const hostRouter = express.Router();
-const path = require("path");
+// Core Module
+const path = require('path');
 
-// local module
+// External Module
+const express = require('express');
+const hostRouter = express.Router();
+
+// Local Module
 const rootDir = require("../utils/pathUtil");
 
 hostRouter.get("/add-home", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "addHome.html"));
-});
+  res.sendFile(path.join(rootDir, 'views', 'addHome.html'));
+})
 
 hostRouter.post("/add-home", (req, res, next) => {
-  console.log(req.body);
-  res.sendFile(path.join(rootDir, "views", "homeAdded.html"));
-});
+  res.sendFile(path.join(rootDir, 'views', 'homeAdded.html'));
+})
 
 module.exports = hostRouter;
