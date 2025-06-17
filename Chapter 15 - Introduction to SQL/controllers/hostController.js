fc032c7fed44564052a0ea104920544847108b62
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-06-17 22:48:42 +0530

    updated

--- Chapter 15 - Introduction to SQL/controllers/hostController.js
+++ Chapter 15 - Introduction to SQL/controllers/hostController.js
@@ -12,7 +12,8 @@ exports.getEditHome = (req, res, next) => {
   const homeId = req.params.homeId;
   const editing = req.query.editing === "true";
 
-  Home.findById(homeId, (home) => {
+  Home.findById(homeId).then(([homes]) => {
+    const home = homes[0];
     if (!home) {
       console.log("Home not found for editing.");
       return res.redirect("/host/host-home-list");
