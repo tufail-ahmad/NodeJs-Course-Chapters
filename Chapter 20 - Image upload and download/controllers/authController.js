fbeb0e9304a7a9cd1a7f77ba67ceada69950cb7a
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2025-06-22 08:38:44 +0530

    chapter 20 has been started

---
+++ Chapter 20 - Image upload and download/controllers/authController.js
@@ -0,0 +1,164 @@
+const { check, validationResult } = require("express-validator");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    pageTitle: "Login",
+    currentPage: "login",
+    isLoggedIn: false,
+    errors: [],
+    oldInput: { email: "" },
+    user: {},
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Signup",
+    currentPage: "signup",
+    isLoggedIn: false,
+    errors: [],
+    oldInput: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      userType: "",
+    },
+    user: {},
+  });
+};
+
+exports.postSignup = [
+  check("firstName")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First name must be at least 2 characters long")
+    .matches(/^[a-zA-Z]+$/)
+    .withMessage("First name must contain only letters"),
+
+  check("lastName")
+    .matches(/^[a-zA-Z]*$/)
+    .withMessage("Last name must contain only letters"),
+
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
+
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@#$%^&*]/)
+    .withMessage("Password must contain at least one special character"),
+
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+
+  check("userType")
+    .notEmpty()
+    .withMessage("Please select a user type")
+    .isIn(["guest", "host"])
+    .withMessage("Invalid user type"),
+
+  check("terms")
+    .notEmpty()
+    .withMessage("You must accept the terms and conditions")
+    .custom((value, { req }) => {
+      if (value !== "on") {
+        throw new Error("You must accept the terms and conditions");
+      }
+      return true;
+    }),
+
+  (req, res, next) => {
+    const { firstName, lastName, email, password, userType } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Signup",
+        currentPage: "signup",
+        isLoggedIn: false,
+        errors: errors.array().map((err) => err.msg),
+        oldInput: { firstName, lastName, email, password, userType },
+        user: {},
+      });
+    }
+
+    bcrypt
+      .hash(password, 12)
+      .then((hashedPassword) => {
+        const user = new User({
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+          userType,
+        });
+        user.save();
+      })
+      .then(() => {
+        res.redirect("/login");
+      })
+      .catch((err) => {
+        return res.status(422).render("auth/signup", {
+          pageTitle: "Signup",
+          currentPage: "signup",
+          isLoggedIn: false,
+          errors: [err.message],
+          oldInput: { firstName, lastName, email, password, userType },
+          user: {},
+        });
+      });
+  },
+];
+
+exports.postLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).render("auth/login", {
+      pageTitle: "Login",
+      currentPage: "login",
+      isLoggedIn: false,
+      errors: ["Invalid email or password"],
+      oldInput: { email },
+      user: {},
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(422).render("auth/login", {
+      pageTitle: "Login",
+      currentPage: "login",
+      isLoggedIn: false,
+      errors: ["Invalid email or password"],
+      oldInput: { email },
+      user: {},
+    });
+  }
+
+  req.session.isLoggedIn = true;
+  req.session.user = user;
+  await req.session.save();
+  res.redirect("/");
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  });
+};
